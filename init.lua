a94aba384b67426fc940a75f525167b739b0cec4
Jason Nguyen <tri.in.the.sky@gmail.com>
2025-08-02 21:34:56 -0700

    lsp gD and inline warn/hint

--- init.lua
+++ init.lua
@@ -1,8 +1,8 @@
 require("config.lazy")
 require("config.remap")
 require("config.set")
+require("config.lsp")
 
--- import lsp manually
 vim.lsp.enable("lua_ls")
 vim.lsp.enable("gopls")
 vim.lsp.enable("ts_ls")
@@ -11,7 +11,7 @@ vim.lsp.enable("rust_analyzer")
 vim.lsp.enable("gdtoolkit")
 vim.lsp.enable("pylsp")
 
-vim.opt.termguicolors = true 
+vim.opt.termguicolors = true
 
 vim.api.nvim_set_hl(0, "Normal",      { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
